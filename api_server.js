a0259cbcef9bd88d4dd9436d9cdd43c15e92d695
Kevin Sulonen <kevin.sulonen@gmail.com>
2017-02-09 20:15:25 -0800

    Initial commit

---
+++ api_server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let express = require('express');
+let mongoose = require('mongoose');
+let morgan = require('morgan');
+const PORT = process.env.port || 3000;
+
+const MONGO_DB = process.env.MONGO_DB || 'mongodb://localhost/db';
+mongoose.connect(MONGO_DB);
+
+let authRouter = express.Router();
+require('./routes/auth_routes')(authRouter);
+
+let apiRouter = express.Router();
+require('./routes/user_routes')(apiRouter);
+require('./routes/api_routes')(apiRouter);
+
+let app = module.exports = exports = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin',
+             'http://localhost:8080');
+  res.header('Access-Control-Allow-Headers',
+             'Content-Type, Authorization, token');
+  res.header('Access-Control-Allow-Methods',
+             'GET, POST, PUT, DELETE');
+  next();
+});
+
+app.use(morgan('dev'));
+app.use('/', authRouter);
+app.use('/', apiRouter);
+
+app.listen(PORT, () => {
+  console.log('Server listening on port ' + PORT);
+});
